99e4207e63bea7f461748539fa975f7cf99107d3
jdjeffers <jdjeffers@example.com>
2026-04-28 23:13:09 -0400

    feat: set default music volume to 65% and persist audio settings to config file

--- Scripts/MusicManager.gd
+++ Scripts/MusicManager.gd
@@ -7,6 +7,43 @@ func _ready():
 	_player = AudioStreamPlayer.new()
 	add_child(_player)
 	_player.bus = "Music"
+	_load_settings()
+
+func _load_settings():
+	var config = ConfigFile.new()
+	var err = config.load("user://settings.cfg")
+	
+	var music_bus = AudioServer.get_bus_index("Music")
+	var sfx_bus = AudioServer.get_bus_index("SFX")
+	
+	if err == OK:
+		if config.has_section_key("Audio", "music_volume"):
+			var vol = config.get_value("Audio", "music_volume")
+			if music_bus >= 0:
+				AudioServer.set_bus_volume_db(music_bus, linear_to_db(vol))
+		else:
+			if music_bus >= 0:
+				AudioServer.set_bus_volume_db(music_bus, linear_to_db(0.65))
+			config.set_value("Audio", "music_volume", 0.65)
+			config.save("user://settings.cfg")
+			
+		if config.has_section_key("Audio", "sfx_volume"):
+			var vol = config.get_value("Audio", "sfx_volume")
+			if sfx_bus >= 0:
+				AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(vol))
+		else:
+			if sfx_bus >= 0:
+				AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(1.0))
+			config.set_value("Audio", "sfx_volume", 1.0)
+			config.save("user://settings.cfg")
+	else:
+		if music_bus >= 0:
+			AudioServer.set_bus_volume_db(music_bus, linear_to_db(0.65))
+		if sfx_bus >= 0:
+			AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(1.0))
+		config.set_value("Audio", "music_volume", 0.65)
+		config.set_value("Audio", "sfx_volume", 1.0)
+		config.save("user://settings.cfg")
 
 func play_music(stream_path: String, volume_db: float = 0.0, fade_in: float = 0.0):
 	var stream = load(stream_path)
